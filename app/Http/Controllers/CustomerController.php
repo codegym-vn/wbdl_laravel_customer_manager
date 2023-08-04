d865486017beca9560aed5c8df479994ef97c645
Admin <ad@luanphan.local>
2023-08-04 15:45:45 +0700

    init project

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    function index() {
+        return view('customers.list');
+    }
+
+    function create() {
+        return view('customers.create');
+    }
+
+}
